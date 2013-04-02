4aaed502301fd8534c2b4e391708bcd42c8d8980
Ondrej Cermak <cermak.cz@gmail.com>
2013-04-02 10:44:23 +0200

    Fixed referencing of inner classes with '.' instead of '$'.

--- compiler/src/main/java/dagger/internal/codegen/GeneratorKeys.java
+++ compiler/src/main/java/dagger/internal/codegen/GeneratorKeys.java
@@ -120,7 +120,16 @@ final class GeneratorKeys {
    * runtime.
    */
   static String getCodeGeneratedMembersKey(TypeMirror type) {
-    return "\"members/\" + " + CodeGen.rawTypeToString(type, '$') + ".class.getCanonicalName()";
+    return "\"members/\" + " + CodeGen.rawTypeToString(type, '.') + ".class.getCanonicalName()";
+  }
+
+  /**
+   * Returns the code for generating the provider key for {@code type} in runtime.
+   */
+  public static String getCodeGeneratedProviderKey(TypeMirror type) {
+    StringBuilder result = new StringBuilder();
+    CodeGen.typeToString(type, result, '.');
+    return result.toString();
   }
 
 	/**
@@ -144,7 +153,7 @@ final class GeneratorKeys {
       qualifierToStringCodeGenerated(qualifier, result);
       result.append(" + ");
     }
-    CodeGen.typeToString(variable.asType(), result, '$');
+    CodeGen.typeToString(variable.asType(), result, '.');
     result.append(".class.getCanonicalName()");
     return result.toString();
   }
@@ -159,7 +168,7 @@ final class GeneratorKeys {
       qualifierToStringCodeGenerated(qualifier, result);
       result.append(" + ");
     }
-    CodeGen.typeToString(method.getReturnType(), result, '$');
+    CodeGen.typeToString(method.getReturnType(), result, '.');
     result.append(".class.getCanonicalName()");
     return result.toString();
   }
@@ -178,7 +187,7 @@ final class GeneratorKeys {
     result.append("\"");
     result.append(SET_PREFIX);
     result.append("\" + ");
-    CodeGen.typeToString(method.getReturnType(), result, '$');
+    CodeGen.typeToString(method.getReturnType(), result, '.');
     result.append(".class.getCanonicalName()");
     result.append("+ \"");
     result.append(">");
@@ -195,7 +204,7 @@ final class GeneratorKeys {
     result.append("\"");
     result.append('@');
     result.append("\" + ");
-    CodeGen.typeToString(qualifier.getAnnotationType(), result, '$');
+    CodeGen.typeToString(qualifier.getAnnotationType(), result, '.');
     result.append(".class.getCanonicalName()");
     result.append("+ \"");
     result.append('(');
