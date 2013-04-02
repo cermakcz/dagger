c9bfc8a73c9ea81e6308b46092e704bfb8563c7c
Ondrej Cermak <cermak.cz@gmail.com>
2013-04-02 13:18:54 +0200

    Added test for generic type injection.

---
+++ compiler/src/it/inject-generic-type/src/main/java/test/TestApp.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2013 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.inject.Inject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import dagger.Module;
+import dagger.ObjectGraph;
+import dagger.Provides;
+
+/**
+ * Tests if we can inject two generic types differentiated only by their type parameters.
+ *
+ * @author Ondrej Cermak (cermak.cz@gmail.com)
+ */
+class TestApp implements Runnable {
+  @Inject
+  Map<String, Integer> stringMap;
+  Map<Float, Integer> floatMap;
+
+  @Override public void run() {
+    // Yay! \o/
+  }
+
+  public static void main(String[] args) {
+    ObjectGraph.create(new TestModule()).get(TestApp.class).run();
+  }
+
+  @Module(entryPoints = TestApp.class)
+  static class TestModule {
+    @Provides Map<String, Integer> provideStringMap() {
+      return new HashMap<String, Integer>();
+    }
+
+    @Provides Map<Float, Integer> provideFloatMap() {
+      return new HashMap<Float, Integer>();
+    }
+  }
+}
